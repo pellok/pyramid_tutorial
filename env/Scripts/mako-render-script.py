6adc69ad769409337e17dbba79c2b33183cd107f
pellok <pellok2002@gmail.com>
2015-10-20 21:47:33 +0800

    environment

---
+++ env/Scripts/mako-render-script.py
@@ -0,0 +1,10 @@
+#!D:\Python\Pyramid\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'mako==1.0.2','console_scripts','mako-render'
+__requires__ = 'mako==1.0.2'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('mako==1.0.2', 'console_scripts', 'mako-render')()
+    )
